fa91698dac2ac1705220b53f7531e35c01131fc4
Perrault <lucas.perrault@hotmail.com>
2020-12-15 15:41:42 +0100

    ADD actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,20 @@
-/* All const actions */
+import {getSongsAsync} from "../song.service";
 
+export const UPDATE_AUTOCOMPLETE_SONGS = "UPDATE_AUTOCOMPLETE_SONGS";
+export const UPDATE_SELECTED_SONGS = "UPDATE_SELECTED_SONGS";
+export const UPDATE_MODAL = "UPDATE_MODAL";
 
+export const updateAutocompleteSongs = async (dispatch, searchValue) => {
+    const response = await getSongsAsync(searchValue)
+    dispatch({type: UPDATE_AUTOCOMPLETE_SONGS, payload: response})
+}
 
-/* All dispatches methods */
+export const updateSelectedSongs = selectedSongs => ({
+    type: UPDATE_SELECTED_SONGS,
+    payload: selectedSongs
+});
+
+export const updateModal = isOpen => ({
+    type: UPDATE_MODAL,
+    payload: isOpen
+});
