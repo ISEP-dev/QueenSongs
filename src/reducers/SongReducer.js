6b4d15fbe05cbc76156498519927b0e6683f678f
Perrault <lucas.perrault@hotmail.com>
2020-12-15 15:42:01 +0100

    ADD reducer

--- src/reducers/SongReducer.js
+++ src/reducers/SongReducer.js
@@ -1,9 +1,30 @@
-const defaultState = {
+import {UPDATE_AUTOCOMPLETE_SONGS, UPDATE_MODAL, UPDATE_SELECTED_SONGS} from "../actions";
 
+const defaultState = {
+    isOpen: false,
+    autocompleteSongs: [],
+    selectedSongs: []
 }
 
-export const songReducer = (state = defaultState, payload) => {
-    switch (state) {
+export const songReducer = (state = defaultState, action) => {
+    switch (action.type) {
+        case UPDATE_AUTOCOMPLETE_SONGS:
+            return {
+                ...state,
+                autocompleteSongs: action.payload,
+                selectedSongs: state.selectedSongs
+            }
+        case UPDATE_SELECTED_SONGS:
+            return {
+                isOpen: state.isOpen,
+                autocompleteSongs: state.autocompleteSongs,
+                selectedSongs: action.payload
+            }
+        case UPDATE_MODAL:
+            return {
+                ...state,
+                isOpen: action.payload
+            }
         default:
             return state;
     }
